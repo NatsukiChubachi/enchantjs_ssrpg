b996f61ad7be675b063f05232be3754a62388ed9
chubachinatsuki <chubachinatsuki@192.168.2.105>
2015-09-27 21:27:01 +0900

    ターンステートを追加

--- ssrpg/js/tcg/tcg_battlefield.js
+++ ssrpg/js/tcg/tcg_battlefield.js
@@ -5,6 +5,20 @@
  */
 
 var _gTcgBattleField = {
+    OwnerNumber: {
+        iNoneState: -1,
+        iPlayerA: 0,
+        iPlayerB: 1
+    },
+    TurnState: {
+        iNoneState: -1,
+        iBattleStart: 0,
+        iRefreshState: 1,
+        iDrawState: 2,
+        iMainState: 3,
+        iBattleState: 4,
+        iEndState: 5
+    },
     CardChip: {
         iSizeX: 40,
         iSizeY: 60
@@ -25,22 +39,141 @@ var CTcgBattleField = function() {
         this._scene.backgroundColor = "#999999";
         
         // ラベル作成
-        this.lblPage = this.createLabel( 10, 10, "TCG BattleField Page." );
-        this.lblPage.font = "32px cursive";
+        this._lblPage = this.createLabel( 10, 10, "TCG BattleField Page." );
+        this._lblPage.font = "32px cursive";
 
         // 作成したシーンを追加する
         this._game.pushScene( this._scene );
         
         // カードチップの試作
-        this.createLabel( 10, 450, "Player_A Life = 20" );
-        this.createLabel( 10, 470, "Player_B Life = 20" );
+        this._lblLifeA = this.createLabel( 10, 450, "Player_A Life = 20" );
+        this._lblLifeB = this.createLabel( 10, 470, "Player_B Life = 20" );
+        
+        // ターン制御変数
+        this._iOwnerState = _gTcgBattleField.OwnerNumber.iPlayerA;
+        this._iTurnState  = _gTcgBattleField.TurnState.iBattleStart;
+        this._bStateMove = false;
+        
+        // 毎ループ判定処理
+        this._lblManager = this.createLabel( 0, 0, "!" );
+        this._lblManager._parent = this;
+        this._lblManager.addEventListener("enterframe", function(){
+
+            switch( this._parent._iTurnState )
+            {
+            case _gTcgBattleField.TurnState.iBattleStart:
+                alert("BattleStart");
+                this._parent._iTurnState = _gTcgBattleField.TurnState.iRefreshState;
+                break;
+            case _gTcgBattleField.TurnState.iRefreshState:
+                if ( this._parent._bStateMove )
+                {
+                    alert("RefreshState");
+                    this._parent._iTurnState = _gTcgBattleField.TurnState.iDrawState;
+                    this._parent._bStateMove = false;
+                }
+                break;
+            case _gTcgBattleField.TurnState.iDrawState:
+                if ( this._parent._bStateMove )
+                {
+                    alert("DrawState");
+                    this._parent._iTurnState = _gTcgBattleField.TurnState.iMainState;
+                    this._parent._bStateMove = false;
+                }
+                break;
+            case _gTcgBattleField.TurnState.iMainState:
+                if ( this._parent._bStateMove )
+                {
+                    alert("MainState");
+                    this._parent._iTurnState = _gTcgBattleField.TurnState.iBattleState;
+                    this._parent._bStateMove = false;
+                }
+                break;
+            case _gTcgBattleField.TurnState.iBattleState:
+                if ( this._parent._bStateMove )
+                {
+                    alert("BattleState");
+                    this._parent._iTurnState = _gTcgBattleField.TurnState.iEndState;
+                    this._parent._bStateMove = false;
+                }
+                break;
+            case _gTcgBattleField.TurnState.iEndState:
+                if ( this._parent._bStateMove )
+                {
+                    alert("EndState");
+                    this._parent._iTurnState = _gTcgBattleField.TurnState.iRefreshState;
+                    this._parent._bStateMove = false;
+                    
+                    if ( this._parent._iOwnerState === _gTcgBattleField.OwnerNumber.iPlayerA )
+                    {
+                        this._parent._iOwnerState = _gTcgBattleField.OwnerNumber.iPlayerB;
+                    }
+                    else
+                    {
+                        this._parent._iOwnerState = _gTcgBattleField.OwnerNumber.iPlayerA;
+                    }
+                }
+                break;
+            };
+            
+        });
+        
+        // ターン状態表示ラベル
+        this._lblTurnState = this.createLabel( 250, 450, "TurnState: " );
+        this._lblTurnState._parent = this;
+        this._lblTurnState.addEventListener("enterframe", function() {
+            var sTmp = "TurnState: ";
+            
+            switch( this._parent._iOwnerState )
+            {
+            case _gTcgBattleField.OwnerNumber.iPlayerA:
+                sTmp += "PlayerA, ";
+                break;
+            case _gTcgBattleField.OwnerNumber.iPlayerB:
+                sTmp += "PlayerB, ";
+                break;
+            };
+            
+            switch( this._parent._iTurnState )
+            {
+            case _gTcgBattleField.TurnState.iBattleStart:
+                sTmp += "BattleStart";
+                break;
+            case _gTcgBattleField.TurnState.iRefreshState:
+                sTmp += "RefreshState";
+                break;
+            case _gTcgBattleField.TurnState.iDrawState:
+                sTmp += "DrawState";
+                break;
+            case _gTcgBattleField.TurnState.iMainState:
+                sTmp += "MainState";
+                break;
+            case _gTcgBattleField.TurnState.iBattleState:
+                sTmp += "BattleState";
+                break;
+            case _gTcgBattleField.TurnState.iEndState:
+                sTmp += "EndState";
+                break;
+            };
+            
+            this.text = sTmp;
+        } );
+        
+        // テンポラリ変数
+        var tmp;
         
         // Player_A 手札
         this.createChip( 100, 370 );
         this.createChip( 150, 370 );
         this.createChip( 200, 370 );
         this.createChip( 250, 370 );
-        this.createChip( 300, 370 );
+        tmp = this.createChip( 300, 370 );
+        tmp._parent = this;
+        tmp.addEventListener("touchstart", function() {
+          // alert("AAA");
+          this.y += 1;
+          this._parent._bStateMove = true;
+        });
         
         // Player_A 場
         this.createChip( 100, 260 );
@@ -59,6 +192,33 @@ var CTcgBattleField = function() {
         this.createChip( 10, 250 );
         this.createChip( 10, 300 );
         this.createChip( 10, 350 );
+        
+        
+        // Player_B 手札
+        this.createChip( 100, 120 - 60 );
+        this.createChip( 150, 120 - 60 );
+        this.createChip( 200, 120 - 60 );
+        this.createChip( 250, 120 - 60 );
+        this.createChip( 300, 120 - 60 );
+        
+        // Player_B 場
+        this.createChip( 300, 240 - 60 );
+        this.createChip( 250, 240 - 60 );
+        this.createChip( 200, 240 - 60 );
+        
+        // Player_B 山札
+        this.createChip( 30, 250 - 60 );
+        this.createChip( 35, 245 - 60 );
+        this.createChip( 40, 240 - 60 );
+
+        // Player_B 捨て山
+        this.createChip( 30, 150 - 60 );
+        
+        // Player_B プレイヤー情報
+        this.createChip( 410, 250 - 60 );
+        this.createChip( 410, 200 - 60 );
+        this.createChip( 410, 150 - 60 );
+        
     };
     
     // ラベルの作成
@@ -76,7 +236,7 @@ var CTcgBattleField = function() {
     };
     
     // カードチップの作成
-    this.createChip = function(iPosX, iPosY)
+    this.createChip = function( iPosX, iPosY ) 
     {
         var iSizeX = _gTcgBattleField.CardChip.iSizeX;
         var iSizeY = _gTcgBattleField.CardChip.iSizeY;
@@ -97,6 +257,8 @@ var CTcgBattleField = function() {
         }
         
         this._scene.addChild( _field );
+        
+        return _field;
     };
     
     return this;
